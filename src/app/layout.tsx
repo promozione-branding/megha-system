6d4647f5c25849266b06ef80a98f2674d4d07008
manish kumar <mk7219630@gmail.com>
2026-09-21 11:17:56 +0530

    code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,22 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             __html: JSON.stringify(jsonLd),
           }}
         />
+         {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);
+                t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ylmkevm6z3");
+          `}
+        </Script>
       </head>
 
       <body className="min-h-full flex flex-col">{children}</body>
